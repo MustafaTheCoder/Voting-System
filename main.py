b6bb9df30da8c30846bb54c83bea7206f9b2f238
MustafaXD <73116655+MustafaTheCoder@users.noreply.github.com>
2021-10-23 20:15:44 +0500

    Added Algorithm

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-dec = input("What is the Decision?")
+dec = input("What is the Decision?\n")
 
-vote1 = input("Are You Confirm? Yes/No")
-vote2 = input("Are You Confirm? Yes/No")
+vote1 = input("How many people vote (Yes)?\n")
+vote2 = input("How many people vote (No)?\n")
+
+if vote1 > vote2:
+    print(f"{dec}, Is Decided!")
+else:
+    print(f"{dec}, Isn't Decided!")
